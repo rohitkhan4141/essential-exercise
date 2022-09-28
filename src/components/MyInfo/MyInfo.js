cef135394b3af53e6887a2b818c2a1240708fc15
rohitkhan4141 <rohitkhan4141@gmail.com>
2022-09-28 23:51:11 +0600

    myInfo component added

--- src/components/MyInfo/MyInfo.js
+++ src/components/MyInfo/MyInfo.js
@@ -1,7 +1,15 @@
 import React from "react";
+import logo from "../../images/img.png";
+import "./MyInfo.css";
 
 const MyInfo = () => {
-  return <div>my info compo</div>;
+  return (
+    <div className='my-info'>
+      <img src={logo} alt='' />
+      <h3>Mostakin Ahmed Khan</h3>
+      <p>Full-stack Developer</p>
+    </div>
+  );
 };
 
 export default MyInfo;
