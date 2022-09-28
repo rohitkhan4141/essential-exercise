2ed0249ecc3beb8ad1e40f4701fb5cf57a70454f
rohitkhan4141 <rohitkhan4141@gmail.com>
2022-09-29 01:20:24 +0600

    display Exercise Info added

--- src/components/DisplayExerciseInfo/DisplayExerciseInfo.js
+++ src/components/DisplayExerciseInfo/DisplayExerciseInfo.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./DisplayExerciseInfo.css";
 
 const DisplayExerciseInfo = () => {
-  return <div>heloo form diplay exercide component</div>;
+  return (
+    <div className='DisplayExerciseInfo'>
+      <h2>Exercise Details</h2>
+      <h5 className='display-exercise-time'>Exercise Time : </h5>
+      <h5 className='display-exercise-time'>Break Time : </h5>
+    </div>
+  );
 };
 
 export default DisplayExerciseInfo;
